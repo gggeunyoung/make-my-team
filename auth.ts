c0c3702d858a242869c6b189aad79f982732a248
theoyun@naver.com <gykim0330@naver.com>
2026-04-01 17:52:35 +0900

    fix error login

--- auth.ts
+++ auth.ts
@@ -1,13 +1,17 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
+const googleClientId =
+  process.env.AUTH_GOOGLE_ID ?? process.env.GOOGLE_CLIENT_ID;
+const googleClientSecret =
+  process.env.AUTH_GOOGLE_SECRET ?? process.env.GOOGLE_CLIENT_SECRET;
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  debug: process.env.NODE_ENV === "development",
   providers: [
     Google({
-      clientId:
-        process.env.AUTH_GOOGLE_ID ?? process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret:
-        process.env.AUTH_GOOGLE_SECRET ?? process.env.GOOGLE_CLIENT_SECRET ?? "",
+      clientId: googleClientId ?? "",
+      clientSecret: googleClientSecret ?? "",
     }),
   ],
   trustHost: true,
