d9eaea7a51ed865afe2a7335ab41f9a0b45b0ffa
theoyun@naver.com <gykim0330@naver.com>
2026-04-01 16:42:52 +0900

    manujeol

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>⚽️ 햄토스트 팀 분석 서비스 시작! (근영님 파이팅)</h1>
+      <h1>⚽️ 햄토스트 팀 분석 서비스 시작! 오늘 만우절이다다</h1>
     </div>
   );
 }
